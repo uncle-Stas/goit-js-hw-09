3db7ab3fe2753687d228a450bbfd0b8e19721284
uncle-Stas <101670941+uncle-Stas@users.noreply.github.com>
2022-08-01 16:18:05 +0000

    Deploying to gh-pages from @ uncle-Stas/goit-js-hw-09@4345d0e72896c65cd9cf220c9aeb1eef78687e6b 🚀

---
+++ 01-color-switcher.c4e616cb.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]"),body:document.querySelector("body")},e=null;t.btnStart.addEventListener("click",(function(){t.btnStart.setAttribute("disabled",""),e=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStart.removeAttribute("disabled"),clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.c4e616cb.js.map
