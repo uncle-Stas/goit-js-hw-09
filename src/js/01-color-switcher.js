4345d0e72896c65cd9cf220c9aeb1eef78687e6b
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-08-01 19:17:09 +0300

    task1 complited

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let colorInterval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.btnStart.addEventListener('click', startChangeBodyColor);
+refs.btnStop.addEventListener('click', stopChangeBodyColor);
+
+function startChangeBodyColor() {
+  refs.btnStart.setAttribute('disabled', '');
+
+  colorInterval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChangeBodyColor() {
+  refs.btnStart.removeAttribute('disabled');
+
+  clearInterval(colorInterval);
+}
